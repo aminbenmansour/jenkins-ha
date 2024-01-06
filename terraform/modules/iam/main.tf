b9ea542f283f602022922ecc4a1161447413496d
aminbenmansour <amin.benmansour.10@gmail.com>
2024-01-06 02:26:23 +0100

    chore: add instance profile

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -39,3 +39,8 @@ resource "aws_iam_policy_attachment" "jenkins_role_policy_attachment" {
   policy_arn = aws_iam_policy.jenkins_iam_policy.arn
   roles      = [aws_iam_role.jenkins_role.name]
 }
+
+resource "aws_iam_instance_profile" "jenkins_instance_profile" {
+  name = var.instance_profile_name
+  role = aws_iam_role.jenkins_role.name
+}

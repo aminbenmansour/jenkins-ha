711fb8f6e330a90e5a619d64dc62aa2279513fcb
aminbenmansour <amin.benmansour.10@gmail.com>
2024-01-08 22:31:12 +0100

    feat(alb): create alb listener and tg

--- terraform/modules/lb-asg/main.tf
+++ terraform/modules/lb-asg/main.tf
@@ -34,3 +34,36 @@ resource "aws_lb" "jenkins" {
   }
 }
 
+resource "aws_lb_target_group" "jenkins" {
+  name_prefix = "jks-lb"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "instance"
+
+  health_check {
+    path                = "/login"
+    port                = 8080
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Environment = var.environment
+    Terraform   = "true"
+  }
+}
+
+resource "aws_lb_listener" "jenkins" {
+  load_balancer_arn = aws_lb.jenkins.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.jenkins.arn
+    type             = "forward"
+  }
+}
--- terraform/modules/lb-asg/variables.tf
+++ terraform/modules/lb-asg/variables.tf
@@ -2,6 +2,10 @@ variable "environment" {
   description = "The environment name for the resources."
 }
 
+variable "vpc_id" {
+  description = "The ID of the VPC to use for the resources."
+}
+
 variable "subnets" {
   description = "A list of subnet IDs to use for the resources."
   type        = list(string)

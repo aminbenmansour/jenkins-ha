46f9660bda793e7555f2aa2738458c6779aa2b66
aminbenmansour <amin.benmansour.10@gmail.com>
2024-01-08 23:18:13 +0100

    fix(tf module): define required vars to tf plan changes

--- terraform/modules/lb-asg/variables.tf
+++ terraform/modules/lb-asg/variables.tf
@@ -1,3 +1,15 @@
+variable "ami_id" {
+  description = "The ID of the Amazon Machine Image (AMI) to use for the EC2 instances."
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance to use for the ASG."
+}
+
+variable "key_name" {
+  description = "The name of the EC2 key pair to use for the instances."
+}
+
 variable "environment" {
   description = "The environment name for the resources."
 }
